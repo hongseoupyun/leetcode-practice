71a109ed845fb5f0d3d089d9334882e773bce02a
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2026-08-26 10:01:01 -0400

    feat: add comments about linked list definition for better understanding

--- Two Pointers/206. Reverse Linked List.ts
+++ Two Pointers/206. Reverse Linked List.ts
@@ -22,6 +22,17 @@ Time Complexity: O(n) - As we are iterating through the linked list once.
 Space Complexity: O(1) - As we are not using any extra space.
 */
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
 function reverseList(head: ListNode | null): ListNode | null {
     // prev will point to the previous node, curr will point to the current node
